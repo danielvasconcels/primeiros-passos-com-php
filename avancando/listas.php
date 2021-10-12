897b421beea22a7fb45db2f19eba73d2aad87df8
danielvasconcels <86674795+danielvasconcels@users.noreply.github.com>
2021-10-12 01:20:38 -0300

    Create listas.php

---
+++ avancando/listas.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+$idade1 = 21;
+$idade2 = 23;
+$idade3 = 19;
+$idade4 = 21;
+$idade5 = 20;
+$idade6 = 25; */
+
+$idadeList = [21, 23, 19, 21, 20, 25];
+$umaIdade = $idadeList[0];
+
+echo $umaIdade;
+
