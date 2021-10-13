a4ac23539a1f75e67be9406243bbeeec852fd599
danielvasconcels <86674795+danielvasconcels@users.noreply.github.com>
2021-10-13 20:25:26 -0300

    array

--- avancando/foreach.php
+++ avancando/foreach.php
@@ -1,20 +1,25 @@
 <?php
 
 $contasCorrentes = [
-   12345678910 => [
-    'titular'  => 'Vinicius',
-    'saldo' => 1000
-],
-   12345567890 => [
-    'titular' => 'Maria',
-    'saldo' => 10000
-],
-   23445566677 => [
-    'titular' => 'Alberto',
-    'saldo' => 300
-]
+    '123.456.789-10' => [
+        'titular'  => 'Vinicius',
+        'saldo' => 1000
+    ],
+    '123.455.678-90' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    '234.455.666-77' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ]
+];
+
+$contasCorrentes['123.258.852-12'] = [
+    'titular' => 'Claudia',
+    'saldo' => 2000
 ];
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $cpf . PHP_EOL;
+    echo $cpf . " " . $conta['titular'] . PHP_EOL;
 }
