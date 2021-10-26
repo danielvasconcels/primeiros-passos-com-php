344b6ce06609d22c847442dfafd691ddbafbdf23
danielvasconcels <86674795+danielvasconcels@users.noreply.github.com>
2021-10-26 20:45:55 -0300

    html-e-php

--- avancando/banco.php
+++ avancando/banco.php
@@ -27,12 +27,28 @@ unset($contasCorrentes['123.456.789-11']);
 
 titularComLetrasMaiusculas($contasCorrentes['123.256.789-12']);
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas correntes</h1>
 
-foreach ($contasCorrentes as $cpf => $conta) {
-    ['titular' => $titular, 'saldo' => $saldo] = $conta;
-    exibeMensagem(
-        "$cpf  $titular  $saldo"
-    );
+    <dl>
+        <?php foreach($contasCorrentes as $cpf => $conta) { ?>
+        <dt>
+            <h3><?= $conta['titular']; ?> - <?= $cpf; ?></h3>
+        </dt>
+        <dd>
+            Saldo: <?= $conta['saldo']; ?>
+        </dd>
+        <?php } ?>
+    </dl>    
 
-
-}
+</body>
+</html>
--- avancando/funcoes.php
+++ avancando/funcoes.php
@@ -2,7 +2,7 @@
 
 function exibeMensagem($mensagem)
 {
-    echo $mensagem . PHP_EOL;
+    echo $mensagem . '<br>';
 }
 
 function depositar(array $conta, float $valorADepositar): array 
@@ -30,4 +30,10 @@ function titularComLetrasMaiusculas(array &$conta)
 {
     $conta ['titular'] = strtoupper($conta['titular']);
     
+}
+
+function exibeConta(array $conta)
+{
+    ['titular' => $titular, 'saldo' => $saldo] = $conta;
+    echo "<li>Titular: $titular. Saldo: $saldo</li>";
 }
